0c7de23d68491b902c8fd5257facf748fa1313cb
Leox27 <mayurx27@gmail.com>
2026-09-08 01:05:35 +0530

    Understanding about the variables in javascript

---
+++ java_script/02_Variables_and_Data/01_variables.js
@@ -0,0 +1,18 @@
+console.log("Hello world!");
+// let a = 27
+var a = 27
+console.log(a)
+a = 'mayur'
+console.log(a)
+
+/*
+Hello world!
+27
+mayur
+*/
+
+/*
+let 8mayu = 1027 // This will through an error
+console.log(8mayu)
+
+*/
